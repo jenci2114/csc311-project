c41c35cba889cebc05a227dd3b2d10fed2f08979
Jenci <jenci2114@gmail.com>
2022-11-27 17:25:00 -0500

    Ensemble: completed evaluation function

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.impute import KNNImputer
+from utils import evaluate
 
 
 def knn_train_predict(train_data: dict, test_data: dict, full_shape: tuple) -> list:
@@ -32,3 +33,18 @@ def knn_train_predict(train_data: dict, test_data: dict, full_shape: tuple) -> l
         predictions.append(mat[user_id, question_id])
 
     return predictions
+
+
+def ensemble_evaluate(pred1: list, pred2: list, pred3: list,
+                      weight: tuple[int, int, int], test_data: dict) -> float:
+    """
+    Evaluate the ensemble of 3 models, whose predictions are given in
+    pred1, 2, 3.
+    """
+    weight_normalized = [w / sum(weight) for w in weight]
+    final_pred = [weight_normalized[0] * p1 +
+                  weight_normalized[1] * p2 +
+                  weight_normalized[2] * p3
+                  for p1, p2, p3 in zip(pred1, pred2, pred3)]
+
+    return evaluate(test_data, final_pred)
