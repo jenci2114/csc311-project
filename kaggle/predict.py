5e0ae51a05028902f56b5388ba900bbe89502741
Jenci <jenci2114@gmail.com>
2022-12-01 17:20:01 -0500

    Kaggle prediction update to without injection

--- kaggle/predict.py
+++ kaggle/predict.py
@@ -3,7 +3,7 @@ This module generates files that can be used to submit
 to the CSC311 Kaggle competition.
 """
 from part_a.item_response import irt
-from part_b.ae_question_inject_by_mul import AutoEncoder, train, evaluate
+from part_b.ae import AutoEncoder, train, evaluate
 from utils import load_train_sparse, load_valid_csv, load_public_test_csv, \
     load_private_test_csv, save_private_test_csv
 import torch
@@ -53,7 +53,7 @@ def generate_prediction(model, out_path, hyper, evaluation=None):
     train_data = hyper['zero_train_data']
     for i, q in enumerate(private_test['question_id']):
         inputs = Variable(train_data[:, q]).unsqueeze(0)
-        output = model(inputs, hyper['betas'][q], hyper['thetas'])
+        output = model(inputs, hyper['betas'][q])
 
         if output[0][private_test['user_id'][i]].item() >= 0.5:
             predictions.append(1.)
@@ -76,24 +76,20 @@ if __name__ == '__main__':
         iterations=25
     )
 
-    thetas = torch.tensor(thetas, dtype=torch.float32)
-
-    k = 10
+    k = 50
     model = AutoEncoder(train_matrix.shape[0], k, 1)
     hyper = {
-        'lr': 0.1,
+        'lr': 0.01,
         'lamb': 0.001,
         'train_data': train_matrix,
         'zero_train_data': zero_train_matrix,
         'valid_data': valid_data,
-        'num_epoch': 10,
+        'num_epoch': 15,
         'betas': betas,
-        'thetas': thetas
     }
     evaluation = {
         'train_data': zero_train_matrix,
         'valid_data': test_data,
         'betas': betas,
-        'thetas': thetas
     }
-    generate_prediction(model, 'predictions/ae_question_inject_by_mul.csv', hyper, evaluation)
+    generate_prediction(model, 'predictions/ae.csv', hyper, evaluation)
